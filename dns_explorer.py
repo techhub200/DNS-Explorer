336573bd58543e876aa6d3c526212212d260ed72
techhub200 <divyanshupant155@gmail.com>
2026-03-13 22:10:56 +0530

    Exception Handling

--- dns_explorer.py
+++ dns_explorer.py
@@ -1,8 +1,18 @@
 #importing dns library
 import dns.resolver as DNS_Function
 domain= input("Enter the domain name :")
-#DNS_Function sends a query to the DNS Server 
-result=DNS_Function.resolve(domain ,"A")
+#defining types of DNS record 
+record_types = ["A", "AAAA", "MX", "CNAME", "TXT"]
 
-for record in result:
-    print(record)
+for record_type in record_types:
+    print(f"\n--- {record_type} Records ---")
+    try:
+        result = DNS_Function.resolve(domain, record_type)
+        for record in result:
+            print(print(f"  Value: {record}  |  TTL: {result.ttl} seconds"))
+    except DNS_Function.NoAnswer:
+        print(f"No {record_type} records found")
+    except DNS_Function.NXDOMAIN:
+        print("Domain does not exist")
+
+    
